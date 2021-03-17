54316c7de7edeb3cba721d0274987474fa77e294
Caleb A Comstock <c_comstock@rocketmail.com>
2021-03-16 17:55:02 -0700

    cleaned up unused/commented out lines

--- pokemon_api/src/components/PokeDex/PokeDex.jsx
+++ pokemon_api/src/components/PokeDex/PokeDex.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import PokemonList from "../PokemonList/PokemonList";
 import styles from "./PokeDex.module.css";
 
@@ -9,80 +9,51 @@ const PokeDex = (props) => {
 	const [urlForNextFetch, setUrlForNextFetch] = useState(null);
 	const [urlFromLastFetch, setUrlFromLastFetch] = useState(null);
 	const [fetchInitiated, setFetchInitiated] = useState(false);
-	// const [pokemonCount, setPokemonCount] = useState(0);
-	// const [nextPokePageUrl, setNextPokePageUrl] = useState("");
-	const fetchPokemonByUrl = useCallback(
-		(url) => {
-			if (url == null || url === "null" || url === undefined) {
-				if (fetchInitiated) setFetchInitiated(false);
-				return;
-			}
-			if (url === urlFromLastFetch) return;
-			fetch(url)
-				.then((response) => {
-					setUrlFromLastFetch(url);
-					return response.json();
-				})
-				.then((response) => {
-					console.log(response);
-					console.log(`setUrlForNextFetch(${response.next});`);
-					// console.log(
-					// 	`setPokemonListAll([${[
-					// 		...pokemonListAll,
-					// 		response.results,
-					// 	]}]);`
-					// );
-					// setPokemonListAll([...pokemonListAll, response.results]);
-					const tempResults = response.results.map(
-						(item) => item.name
-					);
-					let tempList = [];
-					if (response.previous != null)
-						tempList = [...pokemonListAll, ...tempResults];
-					else tempList = tempResults;
-					console.log("TEMPLIST:  ", tempList);
-					setPokemonListAll(tempList);
-					if (urlForNextFetch !== response.next)
-						setUrlForNextFetch(response.next);
-					if (
-						response.next == null ||
-						response.next === "null" ||
-						response.next === undefined
-					)
-						setFetchInitiated(false);
-				})
-				.catch((err) => {
-					console.log(err);
-				});
+
+	const fetchPokemonByUrl = (url) => {
+		if (url == null || url === "null" || url === undefined) {
+			if (fetchInitiated) setFetchInitiated(false);
+			return;
 		}
-		// },
-		// [pokemonListAll]
-	);
+		if (url === urlFromLastFetch) return;
+		// gotta fetch 'em all
+		fetch(url)
+			.then((response) => {
+				setUrlFromLastFetch(url);
+				return response.json();
+			})
+			.then((response) => {
+				console.log(response);
+				console.log(`setUrlForNextFetch(${response.next});`);
+				const tempResults = response.results.map((item) => item.name);
+				let tempList = [];
+				if (response.previous != null)
+					tempList = [...pokemonListAll, ...tempResults];
+				else tempList = tempResults;
+				console.log("TEMPLIST:  ", tempList);
+				setPokemonListAll(tempList);
+				if (urlForNextFetch !== response.next)
+					setUrlForNextFetch(response.next);
+				if (
+					response.next == null ||
+					response.next === "null" ||
+					response.next === undefined
+				)
+					setFetchInitiated(false);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 	const fetchPokemon = () => {
 		if (fetchInitiated) {
 			setFetchInitiated(false);
 			return;
 		}
-		// gotta catch 'em all
 		if (pokemonListAll.length) setPokemonListAll([]);
 		setFetchInitiated(true);
 		fetchPokemonByUrl(initFetchUrl);
 	};
-	// const fetchPokemon = () => {
-	// 	//gotta catch 'em all
-	// 	fetch("https://pokeapi.co/api/v2/pokemon")
-	// 		.then((response) => {
-	// 			return response.json();
-	// 		})
-	// 		.then((response) => {
-	// 			console.log(response);
-	// 			// const pokemonCount = response.count;
-	// 			setPokemonListAll(response.results);
-	// 		})
-	// 		.catch((err) => {
-	// 			console.log(err);
-	// 		});
-	// };
 	useEffect(() => {
 		if (
 			fetchInitiated &&
@@ -90,15 +61,9 @@ const PokeDex = (props) => {
 			urlForNextFetch !== "null" &&
 			urlForNextFetch !== undefined
 		) {
-			// if (urlForNextFetch != null) fetchPokemonByUrl(urlForNextFetch);
-			// else setFetchInitiated(false);
 			fetchPokemonByUrl(urlForNextFetch);
-			// console.log(
-			// 	`useEffect() => fetchPokemonByUrl(${urlForNextFetch});`
-			// );
 		}
 	});
-	// }, [fetchInitiated, urlForNextFetch, fetchPokemonByUrl]);
 	return (
 		<div className={styles.PokeDex}>
 			<button onClick={fetchPokemon}>Fetch Pokemon</button>
